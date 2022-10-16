cb05170709e2a3979d6003e55b11fc075a3d7173
gabester0 <gceipper@gmail.com>
2022-10-16 11:09:11 -0400

    setup '/' route to send hello world to frontend

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send(JSON.stringify({"response": "Hello World"}));
 });
 
 app.listen(port, () => {
